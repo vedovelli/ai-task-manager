8fce965f30fdd85434f99df89b108d053ca9af30
Fábio Vedovelli <vedovelli@gmail.com>
2025-04-20 08:12:34 -0300

    Aula 3.2: Conhecendo o serviço Turso - SQLite na nuvem e integrando na aplicação

--- app/routes/tasks.tsx
+++ app/routes/tasks.tsx
@@ -1,4 +1,12 @@
 import { TasksList } from "~/features/tasks/tasks-list";
+import { turso } from "~/turso";
+
+export async function loader() {
+  turso.execute(
+    "INSERT INTO users (email, name, password_hash) VALUES ('fabio@vedovelli.com.br', 'Vedovelli', 'dasjdsakjd128912739812jhdsahdsajkhda')"
+  );
+  return {};
+}
 
 export default function () {
   return <TasksList />;
